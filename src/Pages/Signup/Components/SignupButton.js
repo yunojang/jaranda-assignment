1c82b30497bdb6b6d76bd16bb854f8a5f1ae7417
yuno <hanganda23@naver.com>
2021-08-04 04:12:41 +0900

    refactor : input button

--- src/Pages/Signup/Components/SignupButton.js
+++ src/Pages/Signup/Components/SignupButton.js
@@ -2,14 +2,15 @@ import React from 'react';
 import styled from 'styled-components'; 
 import COLOR from 'constant/colorCode';
 
-const ButtonWrap = styled.button`
+const ButtonWrap = styled.input.attrs({type:'button'})`
 height: 44px;
 border-radius: 5px;
 background: ${COLOR.SINGUP_MAIN};
 color: white;
 font-size: 14px;
+font-weight: bold;
 `
 
 export default function Button({children, ...props}) {
-  return <ButtonWrap {...props}>{children}</ButtonWrap>
+  return <ButtonWrap {...props} value={children}></ButtonWrap>
 }
