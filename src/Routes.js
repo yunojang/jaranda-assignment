89ec8f59e3bb6ef5e43dbdb2ad6ab024bb05a14c
yuno <hanganda23@naver.com>
2021-08-03 11:28:28 +0900

    chore : routes setting 변경

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Admin from 'Pages/Admin/Admin';
-import Main from 'Pages/Main/Main';
-import Login from 'Pages/Login/Login';
-import Signup from 'Pages/Signup/Signup';
+
+import { ROUTES_PATH } from 'constant/routesPath';
+import Admin from 'Pages/Admin';
+import Main from 'Pages/Main';
+import Login from 'Pages/Login';
+import Signup from 'Pages/Signup';
 import Header from './Components/header';
+
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Header></Header>
         <Switch>
-          <Route exact path="/" component={Main}></Route>
-          <Route path="/admin" component={Admin}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/signup" component={Signup}></Route>
+          <Route exact path={ROUTES_PATH.MAIN} component={Main}></Route>
+          <Route path={ROUTES_PATH.ADMIN} component={Admin}></Route>
+          <Route path={ROUTES_PATH.LOGIN} component={Login}></Route>
+          <Route path={ROUTES_PATH.SIGNUP} component={Signup}></Route>
         </Switch>
       </Router>
     );
