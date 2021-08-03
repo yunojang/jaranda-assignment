f07298220f426ad2d91c09efa6fda6840f4c0d18
yuno <hanganda23@naver.com>
2021-08-03 21:13:21 +0900

    style : signup layout setting

---
+++ src/Pages/Signup/Components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Input from 'Components/input';
+
+function Card() {
+  return (
+    <div>
+      <Input />-  
+      <Input />- 
+      <Input />- 
+      <Input />
+    </div>
+  )
+}
+
+export default Card;
