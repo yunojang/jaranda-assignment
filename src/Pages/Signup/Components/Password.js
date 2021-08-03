0dbbf06f8b3e70eeed532a225504d9b8545aff65
yuno <hanganda23@naver.com>
2021-08-04 05:23:52 +0900

    feat : validate

--- src/Pages/Signup/Components/Password.js
+++ src/Pages/Signup/Components/Password.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import Input from 'Components/input';
 import { AiOutlineCheck as Check } from 'react-icons/ai';
+
+import Input from 'Components/input';
+import Alert from 'Components/inputAlert';
+import useInput from 'hooks/useInput';
+import { samed } from 'utils/validate';
 
 const CHECK = ['숫자', '특수문자', '영문', '8자리 이상'];
 
@@ -23,6 +27,9 @@ const CheckIcon = styled(Check)`
 `;
 
 function Password(pwdInput) {
+  const samedPwd = samed(pwdInput.value);
+
+  const pwdCheck = useInput('',samedPwd);
 
   const renderCheck = () => {
     return CHECK.map((item, index) => <li key={index}><CheckIcon checked={false}/><span>{item}</span></li>)
@@ -43,7 +50,9 @@ function Password(pwdInput) {
         required
         type='password'
         placeholder='비밀번호 확인'
+        {...pwdCheck}
       />
+      <Alert show={!pwdCheck.isValid}>비밀번호와 다릅니다.</Alert>
     </>
   );
 }
