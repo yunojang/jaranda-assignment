c2b751e5ccff64fe6c2cb68e3d60361626878704
yuno <hanganda23@naver.com>
2021-08-03 23:02:42 +0900

    style : password style setting

--- src/Pages/Signup/Components/Password.js
+++ src/Pages/Signup/Components/Password.js
@@ -1,7 +1,18 @@
 import React from "react";
 import Input from 'Components/input';
+import styled from "styled-components";
 
 const CHECK = ['숫자', '특수문자', '영문', '8자리 이상'];
+
+const CheckList = styled.ul`
+  display: flex;
+  margin-bottom: 25px;
+  padding-left: 5px;
+  li {
+    color: #aaa;
+    margin-right: 10px;
+  }
+`;
 
 function Password() {
   
@@ -15,9 +26,9 @@ function Password() {
         type='password'
         placeholder='비밀번호'
       />
-      <ul>
+      <CheckList>
         {renderCheck()}
-      </ul>
+      </CheckList>
       <Input
         type='password'
         placeholder='비밀번호 확인'
