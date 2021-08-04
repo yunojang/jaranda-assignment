a2d95839336a00e3e1d8dbef60e800cddef0c309
ssong10 <leesy1403@naver.com>
2021-08-04 18:23:26 +0900

    Feat : side navigation

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import Navigation from './Navbar'
+import styled from 'styled-components'
 
-function Main() {
+const MainContainer = styled.div`
+  margin-left: 200px;
+  padding: 50px 20px;
+  text-align:center;
+`
+const BoardTitle = styled.div`
+  font-size: 30px;
+  margin-bottom: 50px;
+`
+
+const BoardItemWrap = styled.div`
+  border: 1px solid gray;
+  margin : 10px;
+  padding: 20px;
+`
+
+const BoardItem = ({board,idx}) => {
   return (
-    <div className="main">
-      <p>Main</p>
-    </div>
+    <BoardItemWrap>{board} {idx+1} 번째 게시글 </BoardItemWrap>
+  )
+}
+function Main({location}) {
+  const [board,setBoard] = useState('')
+  useEffect(()=>{
+    setBoard('')
+  },[location])
+  return (
+    <>
+      <Navigation setBoard={setBoard}/>
+      <MainContainer>
+        <BoardTitle>
+          {board}
+        </BoardTitle>
+        {board && Array(6).fill(0).map((_,idx) => (
+          <BoardItem key={idx} idx={idx} board={board}/>
+        ))}
+      </MainContainer>
+    </>
   );
 }
 
